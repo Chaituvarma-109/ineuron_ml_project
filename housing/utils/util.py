6f5237a7d02e5c539f4d8dd7dc0045b1b1e12842
chaituvarma-109 <chaitanyavarma.u@gmail.com>
2022-10-08 20:48:06 +0530

    added configurtions and component

---
+++ housing/utils/util.py
@@ -0,0 +1,12 @@
+import yaml
+import sys
+
+from housing.Exception.customexception import HousingException
+
+
+def read_yaml(file_path: str) -> dict:
+    try:
+        with open(file_path, 'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise HousingException(e, sys) from e
